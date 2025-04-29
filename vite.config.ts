a7585b7f57c8c9a764ce8c351e38474399961330
Shivaji Chaulagain <shivajichaulagain@gmail.com>
2025-04-29 07:04:50 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
-  base: '/',
+  base: '/shivajichaulagain_website/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
